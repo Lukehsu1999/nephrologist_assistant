0b83e52b5e4a7bcdcf58a7f91fb5c177ed103ee2
Lukehsu <lukehsu1999@gmail.com>
2024-11-20 22:49:04 +0800

    init package nephrologistapp

---
+++ src/nephrologistapp/patients/Patient.java
@@ -0,0 +1,41 @@
+package nephrologistapp.patients;
+public class Patient {
+    private String name;
+    private int age;
+    private double weight;
+    
+    public Patient(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }    
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    // Other methods
+    public String printPatient(){
+        return "Patient Info:\n" + "Name: " + name + "\nAge: " + age + "\nWeight: " + weight;
+    }
+}
---
+++ src/nephrologistapp/treatments/Treatment.java
@@ -0,0 +1,27 @@
+package nephrologistapp.treatments;
+
+import nephrologistapp.patients.Patient;
+
+public class Treatment {
+    private String treatmentName;
+
+    public Treatment(String treatmentName) {
+        this.treatmentName = treatmentName;
+    }
+
+
+    // Getters and setters
+    public String getTreatmentName() {  
+        return treatmentName;
+    }
+
+    public void setTreatmentName(String treatmentName) {
+        this.treatmentName = treatmentName;
+    }
+
+    // Other methods
+    public String treatPatient(Patient patient) {
+        return "Patient " + patient.getName() + " has been treated with " + treatmentName;
+    }
+
+}

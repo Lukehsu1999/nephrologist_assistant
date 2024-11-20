ebebd8fa880882ae0a07d937b088956ee113d0e6
Lukehsu <lukehsu1999@gmail.com>
2024-11-20 22:11:10 +0800

    App.java: test Treatment

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Patient patient = new Patient("John Doe", 25, 75.5);
+        Treatment treatment = new Treatment("Antibiotics");
+        
+        
         System.out.println(patient.printPatient());
-        System.out.println("Hello, World!");
+        System.out.println(treatment.treatPatient(patient));
     }
 }

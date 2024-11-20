497f55c58974cc1fab3939fc4c187a16a23da389
Lukehsu <lukehsu1999@gmail.com>
2024-11-20 22:05:40 +0800

    init getters and setters for Patient class

--- src/Patient.java
+++ src/Patient.java
@@ -9,6 +9,31 @@ public class Patient {
         this.weight = weight;
     }
     // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }    
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    // Other methods
     public String printPatient(){
         return "Name: " + name + "\nAge: " + age + "\nWeight: " + weight;
     }

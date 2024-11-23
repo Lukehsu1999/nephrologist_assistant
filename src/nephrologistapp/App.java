7929f6328ba908322aee481de89dd8ed83a9176f
Lukehsu <lukehsu1999@gmail.com>
2024-11-23 21:44:42 +0800

    App.java: try BufferedReader to read in from csv

--- src/nephrologistapp/App.java
+++ src/nephrologistapp/App.java
@@ -1,6 +1,7 @@
 package nephrologistapp;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 import nephrologistapp.patients.Patient;
 import nephrologistapp.treatments.Treatment;
 import nephrologistapp.treatments.PrednisoneTreatment;
@@ -10,6 +11,12 @@ public class App {
         Patient patient = new Patient("John Doe", 25, 50.0);
         Treatment treatment = new PrednisoneTreatment();
         
+        BufferedReader reader = new BufferedReader(new FileReader("src/nephrologistapp/treatments/prednisone_data/standard_dosage.csv"));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            System.out.println(parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3]);
+        }
         
         System.out.println(patient.printPatient());
         System.out.println(treatment.treatPatient(patient));

f6f278e09901dfd2302ced4d231cdb98784ff5e0
Lukehsu <lukehsu1999@gmail.com>
2024-11-26 22:05:10 +0800

    PrednisoneTreatment: populate reducedDosageTable

--- src/nephrologistapp/treatments/PrednisoneTreatment.java
+++ src/nephrologistapp/treatments/PrednisoneTreatment.java
@@ -36,10 +36,10 @@ public class PrednisoneTreatment extends Treatment{
     // Class attribute: static and final
     // DoseType -> Week -> WeightGroup -> Dose
     private static String standardDosageFilePath = "src/nephrologistapp/treatments/prednisone_data/standard_dosage.csv";
-    
+    private static String reducedDosageFilePath = "src/nephrologistapp/treatments/prednisone_data/reduced_dosage.csv";
     // refactor DosageTable to WeightGroup -> Week -> Dose
     private static final Map<String, Map<WeightGroup, Double>> standardDosageTable;
-    private static final Map<String, Map<WeightGroup, Double>> reducedDosageTable;
+    private static final Map<WeightGroup, Map<String, Double>> reducedDosageTable;
 
 
     // Static initializer block to populate the dosageTable
@@ -47,6 +47,7 @@ public class PrednisoneTreatment extends Treatment{
         System.out.println("PrednisoneTreatment class initialized");
         standardDosageTable = new HashMap<>();
         reducedDosageTable = new HashMap<>();
+
         // Populate the dosageTable here
         try (BufferedReader reader = new BufferedReader(new FileReader(standardDosageFilePath))) {
             // Skip the header line
@@ -67,6 +68,30 @@ public class PrednisoneTreatment extends Treatment{
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // Populate the reducedDosageTable here
+        try (BufferedReader reader = new BufferedReader(new FileReader(reducedDosageFilePath))) {
+            // Skip the header line
+            reader.readLine();
+
+            reducedDosageTable.put(WeightGroup.UNDER_50, new HashMap<String, Double>());
+            reducedDosageTable.put(WeightGroup.MEDIUM, new HashMap<String, Double>());
+            reducedDosageTable.put(WeightGroup.OVER_75, new HashMap<String, Double>());
+            
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String weekString = parts[0];
+                double under50Dose = Double.parseDouble(parts[1]);
+                double mediumDose = Double.parseDouble(parts[2]);
+                double over75Dose = Double.parseDouble(parts[3]);
+                reducedDosageTable.get(WeightGroup.UNDER_50).put(weekString, under50Dose);
+                reducedDosageTable.get(WeightGroup.MEDIUM).put(weekString, mediumDose);
+                reducedDosageTable.get(WeightGroup.OVER_75).put(weekString, over75Dose);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
     }
 
@@ -75,7 +100,7 @@ public class PrednisoneTreatment extends Treatment{
         return Collections.unmodifiableMap(standardDosageTable);
     }
 
-    public static String printEntireDosageTable(Map<String, Map<WeightGroup, Double>> dosageTable) {
+    public static String printEntireDosageTable(Map<WeightGroup, Map<String, Double>> dosageTable) {
         return dosageTable.toString();
     }
 
@@ -89,7 +114,7 @@ public class PrednisoneTreatment extends Treatment{
         double patient_weight = patient.getWeight();
         WeightGroup weightGroup = getWeightGroup(patient_weight);
 
-        return printEntireDosageTable(standardDosageTable);
+        return printEntireDosageTable(reducedDosageTable);
     }
     
 }

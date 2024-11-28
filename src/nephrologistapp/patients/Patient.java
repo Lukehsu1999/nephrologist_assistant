feaee8962e609ec6551c01bec11a84c05a11be1f
Lukehsu <lukehsu1999@gmail.com>
2024-11-28 20:24:38 +0800

    Patient.java: add optional attribute GFR

--- src/nephrologistapp/patients/Patient.java
+++ src/nephrologistapp/patients/Patient.java
@@ -1,13 +1,25 @@
 package nephrologistapp.patients;
+
+import java.util.Optional;
+
 public class Patient {
     private String name;
     private int age;
     private double weight;
+    private Optional<Double> gfr;
     
     public Patient(String name, int age, double weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
+        this.gfr = Optional.empty();
+    }
+
+    public Patient(String name, int age, double weight, Double gfr) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.gfr = Optional.ofNullable(gfr); // Allow null values
     }
     // Getters and setters
     public String getName() {
@@ -33,9 +45,16 @@ public class Patient {
     public void setWeight(double weight) {
         this.weight = weight;
     }
+    public Optional<Double> getGFR() {
+        return gfr;
+    }
+
+    public void setGFR(Double gfr) {
+        this.gfr = Optional.ofNullable(gfr);
+    }
 
     // Other methods
     public String printPatient(){
-        return "Patient Info:\n" + "Name: " + name + "\nAge: " + age + "\nWeight: " + weight;
+        return "Patient Info:\n" + "Name: " + name + "\nAge: " + age + "\nWeight: " + weight + (gfr.isPresent() ? "\nGFR: " + gfr.get() : "\nGFR: Not available");
     }
 }

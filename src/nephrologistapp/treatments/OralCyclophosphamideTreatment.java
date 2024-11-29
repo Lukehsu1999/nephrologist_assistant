1612943f45a52c0ff2233d7d9e383cb41ad01c53
Lukehsu <lukehsu1999@gmail.com>
2024-11-30 00:14:33 +0800

    OralCyclophosphamideTreatment: init

--- src/nephrologistapp/treatments/OralCyclophosphamideTreatment.java
+++ src/nephrologistapp/treatments/OralCyclophosphamideTreatment.java
@@ -1,5 +1,15 @@
 package nephrologistapp.treatments;
 
-public class OralCyclophosphamideTreatment {
+import nephrologistapp.patients.Patient;
+public class OralCyclophosphamideTreatment extends Treatment {
+
+    public OralCyclophosphamideTreatment() {
+        super("Oral Cyclophosphamide");
+    }
+
+    @Override
+    public String treatPatient(Patient patient) {
+        return "Patient " + patient.getName() + " has been treated with Oral Cyclphosphamide";
+    }
     
 }

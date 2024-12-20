b96087c544451b09beba58a19c44073f034273df
Lukehsu <lukehsu1999@gmail.com>
2024-12-20 22:18:56 +0800

    OralCyclophosphamideTreatment: use conditional clauses for prototype

--- src/nephrologistapp/treatments/OralCyclophosphamideTreatment.java
+++ src/nephrologistapp/treatments/OralCyclophosphamideTreatment.java
@@ -9,7 +9,35 @@ public class OralCyclophosphamideTreatment extends Treatment {
 
     @Override
     public String treatPatient(Patient patient) {
-        return "Patient " + patient.getName() + " has been treated with Oral Cyclphosphamide";
+        Double gfr = patient.getGFR().orElse(-1.0);
+        // check if gfr is present
+        if (gfr == -1.0){
+            return "GFR not available. Cannot prescribe Oral Cyclophosphamide";
+        }
+        int age = patient.getAge();
+        double weight = patient.getWeight();
+        if (gfr > 30){
+            if (age<60){
+                return "2 mg/kg/day";
+            }
+            else if (age>=60 && age<70){
+                return "1.5 mg/kg/day";
+            }
+            else {
+                return "1.25 mg/kg/day";
+            }
+        }
+        else {
+            if (age<60){
+                return "1.5 mg/kg/day";
+            }
+            else if (age>=60 && age<70){
+                return "1.25 mg/kg/day";
+            }
+            else {
+                return "1 mg/kg/day";
+            }
+        }
     }
     
 }
